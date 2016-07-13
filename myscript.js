b16a86a1ddee9d3af062a410822dc546d637e1c1
JennyMao <jclw1415@gmail.com>
2016-07-13 16:05:27 -0700

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,44 @@
+var numberOfKittens = 10;
+    var numberOfDogs = 30;
+        
+    addnumbers(numberOfKittens, numberOfDogs);
+    console.log("Starting with example");
+    
+    function addnumbers(a, b)
+    {
+        console.log(a + b);
+    }
+   
+        console.log("Done with example.");
+
+      
+      
+      
+  
+  
+        
+    var JennysName = 'Jenny';
+    var fullname = JennysName + ' Mao';
+    console.log(fullname); //Outputs 'Jenny Mao'
+
+  
+     
+    var kittensName = 'Fluffy ';
+    var fullName = kittensName + ' McDougle';
+    console.log(fullName); //Outputs 'Fluffy McDougle'
+     
+
+   
+    var numberOfKittens = 5;
+    var numberOfPuppies = 4;
+    var numberOfAnimals = numberOfKittens + numberOfPuppies;
+    console.log(numberOfAnimals); 
+      
+var first = 'Jenny';
+var last = ' Mao';
+
+    function add(first, last) 
+    {
+        return first + last;
+    }
+    console.log(add(first, last));   // outputs Jenny Mao
